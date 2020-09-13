4647010398e87ab26bed8eee60b0c272630c544b
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2020-09-12 23:26:25 -0300

    Creado clases CuentaAhorro, Prestamo, TarjetaCredito y enum Divisa

---
+++ src/tpo1/TarjetaCredito.java
@@ -0,0 +1,12 @@
+package tpo1;
+
+/**
+ * Tarjeta de crédito de algún cliente del banco.
+ * 
+ * @author {@literal Julian Dominguez <julian.dominguez@est.fi.uncoma.edu.ar>}
+ * @author {@literal Gabriela Gili <gabriela.gili@est.fi.uncoma.edu.ar>}
+ * @author {@literal Diego P. M. Baltar <diego.baltar@est.fi.uncoma.edu.ar>}
+ */
+public class TarjetaCredito {
+
+}
