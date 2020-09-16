6b520461b54c6a31cd687d267be29ad00c88c44d
Diego Pablo Matías Baltar <dpmbaltar@gmail.com>
2020-09-16 20:39:15 -0300

    Agregado comentarios  CuentaTask

--- src/tpo1/CuentaTask.java
+++ src/tpo1/CuentaTask.java
@@ -3,21 +3,44 @@ package tpo1;
 import java.util.List;
 import java.util.concurrent.Callable;
 
-public class CuentaTask implements Callable<Double>{
-        
-    private Divisa tipoDivisa; // recibe el tipo de moneda
+/**
+ * Tarea para obtener el total de balance de cuentas.
+ * 
+ * @author {@literal Julian Dominguez <julian.dominguez@est.fi.uncoma.edu.ar>}
+ * @author {@literal Gabriela Gili <gabriela.gili@est.fi.uncoma.edu.ar>}
+ * @author {@literal Diego P. M. Baltar <diego.baltar@est.fi.uncoma.edu.ar>}
+ */
+public class CuentaTask implements Callable<Double> {
+
+    /**
+     * Tipo de divisa.
+     */
+    private Divisa tipoDivisa;
+    
+    /**
+     * Los clientes a recorrer.
+     */
     private List<Cliente> clientes;
 
-    public CuentaTask(List<Cliente> clientes, Divisa tipoDivisa){
+    /**
+     * Constructor con lista de clientes y tipo de divisa a computar.
+     * 
+     * @param clientes los clientes
+     * @param tipoDivisa tipo de divisa
+     */
+    public CuentaTask(List<Cliente> clientes, Divisa tipoDivisa) {
         this.clientes = clientes;
         this.tipoDivisa = tipoDivisa;
     }
 
+    /**
+     * Suma el balance total de cuentas de clientes del tipo de divisa dado.
+     */
     @Override
     public Double call() throws Exception {
         Double resultado = Double.valueOf(0);
         int cantClientes = clientes.size();
-        
+
         for (int i = 0; i < cantClientes; i++) {
             CuentaAhorro[] cuentas = clientes.get(i).getCuentasAhorro();
             for (int j = 0; j < cuentas.length; j++) {
